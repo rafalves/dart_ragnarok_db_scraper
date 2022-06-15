e00ba7ca1b1c3b528a4c72d87bc4db6de2075a8b
Rafael Alves da Costa <rafalves.costa@gmail.com>
2022-06-15 20:46:40 -0300

    added file generator

--- lib/dart_ragnarok_scrape_db.dart
+++ lib/dart_ragnarok_scrape_db.dart
@@ -1,20 +1,19 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:http/http.dart';
 import 'package:html/parser.dart';
 import 'package:html/dom.dart';
+import 'package:path/path.dart' as p;
 
 void main() async {
-  print('inicio');
-  print(DateTime.now());
-
   await cardsFullDescription();
 }
 
 Future cardsFullDescription() async {
   List<dynamic>? filterCardDescription(Element td) {
     Element? ul = td.querySelector('ul');
-
     List<Element>? listLi = ul?.children;
-
     var formatedDescriptionList = [];
 
     listLi?.forEach((Element liElement) {
@@ -27,19 +26,19 @@ Future cardsFullDescription() async {
           List<Element>? hasChildrenLi = liInsideLi.children;
 
           for (var li in hasChildrenLi) {
-            text.add(li.text);
+            text.add(li.text.toString());
           }
         }
 
         formatedDescriptionList.add(
           {
-            'conditionalDescription': liElement.firstChild,
-            'description': text,
+            'conditionalDescription': liElement.firstChild.toString(),
+            'description': text.toString(),
           },
         );
       } else {
         formatedDescriptionList.add(
-          {'description': liElement.text},
+          {'description': liElement.text.toString()},
         );
       }
     });
@@ -47,7 +46,7 @@ Future cardsFullDescription() async {
     return formatedDescriptionList;
   }
 
-  String? filterCardName(String? name) {
+  String? isThisACard(String? name) {
     if (name != null && (name.contains('card') || name.contains('Card'))) {
       return name;
     } else {
@@ -61,12 +60,11 @@ Future cardsFullDescription() async {
         'https://db.irowiki.org/db/card-search/?search&full&type=0&sort=1,1'),
   );
 
-  print(response.statusCode);
-
   var document = parse(response.body);
+
   List<dynamic> links = document.querySelectorAll(
       'html > body > div.pageBody > table.bgLtTable > tbody > tr');
-  print(links.length);
+
   List<Element> listWithoutHeaders = [];
   for (var i = 0; i < links.length; i++) {
     if (i == 0 || i % 21 == 0) i++;
@@ -80,7 +78,7 @@ Future cardsFullDescription() async {
         'cardIcon': listWithoutHeaders[i]
             .querySelector('td.bgLtRow3 > img')
             ?.attributes['src'],
-        'cardName': filterCardName(
+        'cardName': isThisACard(
             listWithoutHeaders[i].querySelector('td.bgLtRow1 > a')?.innerHtml),
         'cardImage': listWithoutHeaders[i]
             .querySelector('td.bgLtRow1 > a')
@@ -98,7 +96,7 @@ Future cardsFullDescription() async {
         'cardIcon': listWithoutHeaders[i]
             .querySelector('td.bgLtRow4 > img')
             ?.attributes['src'],
-        'cardName': filterCardName(
+        'cardName': isThisACard(
             listWithoutHeaders[i].querySelector('td.bgLtRow2 > a')?.innerHtml),
         'cardImage': listWithoutHeaders[i]
             .querySelector('td.bgLtRow2  > a')
@@ -108,18 +106,15 @@ Future cardsFullDescription() async {
         'cardDescription': filterCardDescription(
             listWithoutHeaders[i].getElementsByClassName('bgLtRow2.padded')[1]),
       };
-
-      listOfCards.add(elementFormated);
+      if (elementFormated['cardName'] != null) {
+        listOfCards.add(elementFormated);
+      }
     }
-
-    print('----');
-    print('card icon: ${elementFormated['cardIcon']}');
-    print('card name: ${elementFormated['cardName']}');
-    print('card image: ${elementFormated['cardImage']}');
-    print('card gear: ${elementFormated['cardGear']}');
-    print('card description: ${elementFormated['cardDescription']}');
-    print('----');
   }
+  var convertedFileResult = File('${p.current}/text_filtered.json');
+  convertedFileResult.writeAsString(json.encode(listOfCards));
+  var rawFileResult = File('${p.current}/text_response.txt');
+  rawFileResult.writeAsString(response.body);
 }
 
 Future cardsSimpleDescription() async {
