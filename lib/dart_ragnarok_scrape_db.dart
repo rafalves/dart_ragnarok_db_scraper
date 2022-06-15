8cdb48506a8125d0bc048f96ef868eeed625ef4f
Rafael Alves da Costa <rafalves.costa@gmail.com>
2022-06-15 13:12:07 -0300

    added cardsFullDescription function

--- lib/dart_ragnarok_scrape_db.dart
+++ lib/dart_ragnarok_scrape_db.dart
@@ -3,88 +3,186 @@ import 'package:html/parser.dart';
 import 'package:html/dom.dart';
 
 void main() async {
-  Future ragScrape() async {
-    var client = Client();
-    Response response = await client.get(
-      Uri.parse(
-          'https://db.irowiki.org/db/card-search/?search&type=0&sort=1,1'),
-    );
+  print('inicio');
+  print(DateTime.now());
 
-    var document = parse(response.body);
-    List<dynamic> links = document.querySelectorAll(
-        'html > body > div.pageBody > table.bgLtTable > tbody > tr');
-    print(links.length);
-    List<Element> listWithoutHeaders = [];
-    for (var i = 0; i < links.length; i++) {
-      if (i == 0 || i % 21 == 0) i++;
-      listWithoutHeaders.add(links[i]);
-    }
-    List<Map<String, String?>> listOfCards = [];
-    for (var i = 0; i < listWithoutHeaders.length; i++) {
-      var elementFormated = {};
-      if (i % 2 == 0) {
-        elementFormated = {
-          'cardIcon': listWithoutHeaders[i]
-              .querySelector('td.bgLtRow3 > img')
-              ?.attributes['src'],
-          'cardName':
-              listWithoutHeaders[i].querySelector('td.bgLtRow1 > a')?.innerHtml,
-          'cardImage': listWithoutHeaders[i]
-              .querySelector('td.bgLtRow1 > a')
-              ?.attributes['href'],
-          'cardGear':
-              listWithoutHeaders[i].querySelector('td.bgLtRow2')?.innerHtml,
-          'cardDescription': listWithoutHeaders[i]
-              .getElementsByClassName('bgLtRow1.padded')[1]
-              .innerHtml,
-        };
-      } else {
-        elementFormated = {
-          'cardIcon': listWithoutHeaders[i]
-              .querySelector('td.bgLtRow4 > img')
-              ?.attributes['src'],
-          'cardName':
-              listWithoutHeaders[i].querySelector('td.bgLtRow2 > a')?.innerHtml,
-          'cardImage': listWithoutHeaders[i]
-              .querySelector('td.bgLtRow2  > a')
-              ?.attributes['href'],
-          'cardGear':
-              listWithoutHeaders[i].querySelector('td.bgLtRow3')?.innerHtml,
-          'cardDescription': listWithoutHeaders[i]
-              .getElementsByClassName('bgLtRow2.padded')[1]
-              .innerHtml,
-        };
-      }
-
-      print('----');
-      print('card icon: ${elementFormated['cardIcon']}');
-      print('card name: ${elementFormated['cardName']}');
-      print('card image: ${elementFormated['cardImage']}');
-      print('card gear: ${elementFormated['cardGear']}');
-      print('card description: ${elementFormated['cardDescription']}');
-      print('----');
-    }
-    //print('List of Cards lenght: ${listOfCards.length}');
-  }
-
-  await ragScrape();
+  await cardsFullDescription();
 }
 
+Future cardsFullDescription() async {
+  List<dynamic>? filterCardDescription(Element td) {
+    Element? ul = td.querySelector('ul');
 
-//  var elementFormated = {
-//         'cardIcon': listWithoutHeaders[i]
-//             .querySelector('td.bgLtRow4.papped.centered > img')
-//             ?.attributes['src'],
-//         'cardName': listWithoutHeaders[i]
-//             .querySelector('td.bgLtRow2.papped > a')
-//             ?.innerHtml,
-//         'cardImge': listWithoutHeaders[i]
-//             .querySelector('td.bgLtRow2.papped > a')
-//             ?.attributes['href'],
-//         'cardGear': listWithoutHeaders[i]
-//             .querySelector('td.bgLtRow3.papped')
-//             ?.innerHtml,
-//         'cardDescription': listWithoutHeaders[i]
-//             .querySelector('td.bgLtRow2.papped')
-//             ?.innerHtml,
-//       };
+    List<Element>? listLi = ul?.children;
+
+    var formatedDescriptionList = [];
+
+    listLi?.forEach((Element liElement) {
+      List<Element>? hasChildren = liElement.children;
+
+      if (hasChildren.isNotEmpty) {
+        List<String> text = [];
+
+        for (var liInsideLi in hasChildren) {
+          List<Element>? hasChildrenLi = liInsideLi.children;
+
+          for (var li in hasChildrenLi) {
+            text.add(li.text);
+          }
+        }
+
+        formatedDescriptionList.add(
+          {
+            'conditionalDescription': liElement.firstChild,
+            'description': text,
+          },
+        );
+      } else {
+        formatedDescriptionList.add(
+          {'description': liElement.text},
+        );
+      }
+    });
+
+    return formatedDescriptionList;
+  }
+
+  String? filterCardName(String? name) {
+    if (name != null && (name.contains('card') || name.contains('Card'))) {
+      return name;
+    } else {
+      return null;
+    }
+  }
+
+  var client = Client();
+  Response response = await client.get(
+    Uri.parse(
+        'https://db.irowiki.org/db/card-search/?search&full&type=0&sort=1,1'),
+  );
+
+  print(response.statusCode);
+
+  var document = parse(response.body);
+  List<dynamic> links = document.querySelectorAll(
+      'html > body > div.pageBody > table.bgLtTable > tbody > tr');
+  print(links.length);
+  List<Element> listWithoutHeaders = [];
+  for (var i = 0; i < links.length; i++) {
+    if (i == 0 || i % 21 == 0) i++;
+    listWithoutHeaders.add(links[i]);
+  }
+  List<Map<String, dynamic>> listOfCards = [];
+  for (var i = 0; i < listWithoutHeaders.length; i++) {
+    Map<String, dynamic> elementFormated = {};
+    if (i % 2 == 0) {
+      elementFormated = {
+        'cardIcon': listWithoutHeaders[i]
+            .querySelector('td.bgLtRow3 > img')
+            ?.attributes['src'],
+        'cardName': filterCardName(
+            listWithoutHeaders[i].querySelector('td.bgLtRow1 > a')?.innerHtml),
+        'cardImage': listWithoutHeaders[i]
+            .querySelector('td.bgLtRow1 > a')
+            ?.attributes['href'],
+        'cardGear':
+            listWithoutHeaders[i].querySelector('td.bgLtRow2')?.innerHtml,
+        'cardDescription': filterCardDescription(
+            listWithoutHeaders[i].getElementsByClassName('bgLtRow1.padded')[1])
+      };
+      if (elementFormated['cardName'] != null) {
+        listOfCards.add(elementFormated);
+      }
+    } else {
+      elementFormated = {
+        'cardIcon': listWithoutHeaders[i]
+            .querySelector('td.bgLtRow4 > img')
+            ?.attributes['src'],
+        'cardName': filterCardName(
+            listWithoutHeaders[i].querySelector('td.bgLtRow2 > a')?.innerHtml),
+        'cardImage': listWithoutHeaders[i]
+            .querySelector('td.bgLtRow2  > a')
+            ?.attributes['href'],
+        'cardGear':
+            listWithoutHeaders[i].querySelector('td.bgLtRow3')?.innerHtml,
+        'cardDescription': filterCardDescription(
+            listWithoutHeaders[i].getElementsByClassName('bgLtRow2.padded')[1]),
+      };
+
+      listOfCards.add(elementFormated);
+    }
+
+    print('----');
+    print('card icon: ${elementFormated['cardIcon']}');
+    print('card name: ${elementFormated['cardName']}');
+    print('card image: ${elementFormated['cardImage']}');
+    print('card gear: ${elementFormated['cardGear']}');
+    print('card description: ${elementFormated['cardDescription']}');
+    print('----');
+  }
+}
+
+Future cardsSimpleDescription() async {
+  var client = Client();
+  Response response = await client.get(
+    Uri.parse('https://db.irowiki.org/db/card-search/?search&type=0&sort=1,1'),
+  );
+
+  var document = parse(response.body);
+  List<dynamic> links = document.querySelectorAll(
+      'html > body > div.pageBody > table.bgLtTable > tbody > tr');
+  print(links.length);
+  List<Element> listWithoutHeaders = [];
+  for (var i = 0; i < links.length; i++) {
+    if (i == 0 || i % 21 == 0) i++;
+    listWithoutHeaders.add(links[i]);
+  }
+  List<Map<String, dynamic>> listOfCards = [];
+  for (var i = 0; i < listWithoutHeaders.length; i++) {
+    Map<String, dynamic> elementFormated = {};
+    if (i % 2 == 0) {
+      elementFormated = {
+        'cardIcon': listWithoutHeaders[i]
+            .querySelector('td.bgLtRow3 > img')
+            ?.attributes['src'],
+        'cardName':
+            listWithoutHeaders[i].querySelector('td.bgLtRow1 > a')?.innerHtml,
+        'cardImage': listWithoutHeaders[i]
+            .querySelector('td.bgLtRow1 > a')
+            ?.attributes['href'],
+        'cardGear':
+            listWithoutHeaders[i].querySelector('td.bgLtRow2')?.innerHtml,
+        'cardDescription': listWithoutHeaders[i]
+            .getElementsByClassName('bgLtRow1.padded')[1]
+            .innerHtml,
+      };
+      listOfCards.add(elementFormated);
+    } else {
+      elementFormated = {
+        'cardIcon': listWithoutHeaders[i]
+            .querySelector('td.bgLtRow4 > img')
+            ?.attributes['src'],
+        'cardName':
+            listWithoutHeaders[i].querySelector('td.bgLtRow2 > a')?.innerHtml,
+        'cardImage': listWithoutHeaders[i]
+            .querySelector('td.bgLtRow2  > a')
+            ?.attributes['href'],
+        'cardGear':
+            listWithoutHeaders[i].querySelector('td.bgLtRow3')?.innerHtml,
+        'cardDescription': listWithoutHeaders[i]
+            .getElementsByClassName('bgLtRow2.padded')[1]
+            .innerHtml,
+      };
+      listOfCards.add(elementFormated);
+    }
+
+    // print('----');
+    // print('card icon: ${elementFormated['cardIcon']}');
+    // print('card name: ${elementFormated['cardName']}');
+    // print('card image: ${elementFormated['cardImage']}');
+    // print('card gear: ${elementFormated['cardGear']}');
+    // print('card description: ${elementFormated['cardDescription']}');
+    // print('----');
+  }
+  print('List of Cards lenght: ${listOfCards.length}');
+}
